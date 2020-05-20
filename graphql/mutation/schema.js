a343ced1d50586118049446da28a9f8708ccde30
shindepriti <pritishinde34@gmail.com>
2020-05-20 17:44:00 +0530

    ADDED:Mutation Type Define Mutation For Register Return Auth Object

--- graphql/mutation/schema.js
+++ graphql/mutation/schema.js
@@ -8,10 +8,17 @@ type User {
     emailId   : String!
     password  : String!
 }
+type Auth {
+    message : String!
+    success : Boolean! 
+}
 
 type Query {
     message : String!
 }
 
+type Mutation {
+    register(firstName:String!,lastName:String!,emailId:String!,password:String!):Auth
+}
 `
 module.exports = { typeDefs }
